f48de55c60314ce90f1b9d4b8ad03f1035285304
Harshal Waghe <harshalwaghe91@gmail.com>
2026-06-14 12:27:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,34 @@
 import streamlit as st
-from streamlit_webrtc import webrtc_streamer
+from streamlit_webrtc import webrtc_streamer, VideoProcessorBase
+from ultralytics import YOLO
+import av
 
-st.title("Camera Test")
+st.title("Real-Time Object Detection")
+
+@st.cache_resource
+def load_model():
+    return YOLO("yolo11n.pt")
+
+model = load_model()
+
+class YOLOProcessor(VideoProcessorBase):
+    def recv(self, frame):
+        img = frame.to_ndarray(format="bgr24")
+
+        try:
+            results = model.predict(img, verbose=False)
+            annotated = results[0].plot()
+
+            return av.VideoFrame.from_ndarray(
+                annotated,
+                format="bgr24"
+            )
+        except Exception as e:
+            print(e)
+            return frame
 
 webrtc_streamer(
     key="camera",
-    media_stream_constraints={
-        "video": True,
-        "audio": False
-    }
+    video_processor_factory=YOLOProcessor,
+    media_stream_constraints={"video": True, "audio": False},
 )
