7b9a75f054ba8c6bfd4729718f031fb4c88ff538
Harshal Waghe <harshalwaghe91@gmail.com>
2026-06-14 11:37:51 +0530

    Add files via upload

---
+++ app (4).py
@@ -0,0 +1,33 @@
+
+import streamlit as st
+from streamlit_webrtc import webrtc_streamer, VideoProcessorBase
+from ultralytics import YOLO
+import av
+
+st.title("Real-Time Object Detection")
+
+model = YOLO("yolo11n.pt")
+
+class YOLOProcessor(VideoProcessorBase):
+
+    def recv(self, frame):
+
+        img = frame.to_ndarray(format="bgr24")
+
+        results = model(img)
+
+        annotated = results[0].plot()
+
+        return av.VideoFrame.from_ndarray(
+            annotated,
+            format="bgr24"
+        )
+
+webrtc_streamer(
+    key="camera",
+    video_processor_factory=YOLOProcessor,
+    media_stream_constraints={
+        "video": True,
+        "audio": False
+    }
+)
